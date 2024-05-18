183954ace55d3bcf315114e4b8a1dcbb2e1ce02f
sophiasundar <sophiagifta17@gmail.com>
2024-05-18 22:46:39 +0530

    commit

---
+++ src/components/Context/AuthContext.js
@@ -0,0 +1,18 @@
+import React, { createContext, useState } from 'react';
+
+const AuthContext = createContext({
+  isAuthenticated: false,
+  setIsAuthenticated: () => {},
+});
+
+const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthContext, AuthProvider };
